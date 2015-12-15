913f6e652d2383eb9c4d27da4b53a5f6c2040b44
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2015-12-15 01:53:53 -0300

    portfolio, contacto

--- source/js/lib/custom.js
+++ source/js/lib/custom.js
@@ -19,12 +19,13 @@ var app = angular
 			});
 		}
 	])
-	.controller('instagramCtrl', [ '$scope', 'photos',
-		function($scope, photos) {
-			// tweets.get({
-			// 	widgetId: '676261576450641920'
-			// }).success(function(data) {
-			// 	$scope.feed = data;
-			// });
-		}
+	.controller('instagramCtrl', [ '$scope', '$http',
+		function($scope, $http) {
+			var max = 8;
+			var instagram_json = 'https://api.instagram.com/v1/users/41166289/media/recent/?client_id=8a5f05fceb2c42299239597c6ded2f8e&count='+max;
+			$http.get( instagram_json )
+				.success(function(response) {
+					$scope.photos = response.data;
+				});
+			}
 	]);
