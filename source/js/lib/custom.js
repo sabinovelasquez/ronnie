6e0cf1e3ac2bd4417569fe39aaea97eea25c52ce
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2015-12-20 23:18:20 -0300

    correcciones

--- source/js/lib/custom.js
+++ source/js/lib/custom.js
@@ -1,11 +1,22 @@
 'use strict';
 
+function processTweetLinks(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+    text = text.replace(exp, "<a href='$1' target='_blank'>$1</a>");
+    exp = /(^|\s)#(\w+)/g;
+    text = text.replace(exp, "$1<a href='https://twitter.com/search?q=%23$2' target='_blank'>#$2</a>");
+    exp = /(^|\s)@(\w+)/g;
+    text = text.replace(exp, "$1<a href='https://www.twitter.com/$2' target='_blank'>@$2</a>");
+    return text;
+}
+
 var app = angular
 
 	.module('app', ['ngAnimate','ui.bootstrap', 'angular-parallax', 'duScroll', 'ngTweets'])
 	.filter('linky', function ($sce) {
 		return function (str) {
-			return $sce.trustAsHtml(str.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/(http[^\s]+)/g, '<a href="$1" target="_blank">$1</a>'));
+			var tweet = processTweetLinks(str);
+			return $sce.trustAsHtml(tweet);
 		}
 	})
 	.controller('gralCtrl', [ '$scope', '$document', 
